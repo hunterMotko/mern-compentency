009d1ac789c6bcb51193c676c7c0acf44a55b2e1
Hunter Motko <huntermotko@Hunters-MacBook-Pro.local>
2021-07-27 11:29:22 -0400

    donewith profile

--- models/Profile.js
+++ models/Profile.js
@@ -80,7 +80,7 @@ const ProfileSchema = new mongoose.Schema({
             },
             current: {
                 type: Boolean,
-                required: true
+                default:false
             },
             description: {
                 type: String
