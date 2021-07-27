26fea4a67bf5d95d41a131a0cafaa66514584a1a
Hunter Motko <huntermotko@Hunters-MacBook-Pro.local>
2021-07-27 18:53:08 -0400

    controled component on register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
+
 import './App.css';
 
 const App=()=> 
+<Router>
+
     <Fragment>
-      <h1>APP</h1>
+      <Navbar/>
+      <Route exact path='/' component={Landing}/>
+      <section className="container">
+        <Switch>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/register' component={Register}/>
+        </Switch>
+      </section>
     </Fragment>
+</Router>
 
 
 export default App;
