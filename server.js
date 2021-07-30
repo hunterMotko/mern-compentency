b35c7fe86daf325bc6ffa4cc2760e3371f5311a0
Hunter Motko <huntermotko@Hunters-MacBook-Pro.local>
2021-07-30 19:50:39 -0400

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require('path')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -11,7 +11,6 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API Running"));
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
